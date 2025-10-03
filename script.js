83b1f42e26c1b97b44f7b7f9e59443ffd04498a0
J <jostjens@icloud.com>
2025-10-03 18:24:15 +0200

    Refactor input validation and random number generation in the Number Guessing Game

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 var btn = document.getElementById('btn'); //* Variable conected to button
 var output = document.getElementById('output'); //* Variable connected to output
 var reset = document.getElementById('resetbtn');
-var number = [Math.floor(Math.random() * 100)]; // Generate random number
+var number = Math.floor(Math.random() * 100) + 1; // Generate random number between 1-100
 btn.addEventListener('click', function () {
     var input = document.getElementById('userInput').value; //* Connect the value of the user input to if statement
-    if (input == number) {
+    var guess = parseInt(input);
+    
+    // Validate input
+    if (input === '' || isNaN(guess) || guess < 1 || guess > 100) {
+        output.innerHTML = "Please enter a valid number between 1 and 100!";
+        return;
+    }
+    
+    if (guess === number) {
         output.innerHTML = "Correct! The number is ".concat(number);
     }
-    else if (input < number) {
+    else if (guess < number) {
         output.innerHTML = "You guessed too low!";
     }
-    else if (input > number) {
+    else if (guess > number) {
         output.innerHTML = "You guessed too high!";
     }
 });
